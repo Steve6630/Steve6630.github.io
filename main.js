9ff5d977fb93d6f38b9e0301ea3be1c8b4569e9e
Stephen Jacob <stephen4jacob@gmail.com>
2021-06-09 18:59:24 +0530

    My Second Commit

--- main.js
+++ main.js
@@ -38,3 +38,21 @@ modalBtn.addEventListener('click', function() {
 modalClose.addEventListener('click', function() {
     modalBg.classList.remove('bg-active');
 });
+
+var textWrapper = document.querySelector('.center-text');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.center-text .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 2250,
+    delay: (el, i) => 150 * (i+1)
+  }).add({
+    targets: '.center-text',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 2000
+  });
